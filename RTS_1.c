29f5859f415090b6d1245f35bdb9bf1dbcf91f06
Sergey Beketov <Inquirus.inq@gmail.com>
2025-05-12 22:27:11 +0300

    Update RTS_1.c

--- RTS_1.c
+++ RTS_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
-{printf("Beketov Sergey Mikhailovich\nGroup I913B \nTask "1");
+{printf("Beketov Sergey Mikhailovich\nGroup I913B \nTask 1\n");
   printf("Hello World \n");
 return(1);
 }
